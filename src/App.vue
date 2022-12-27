e2e827ebcbfb6bf495c69a942cdb9ee6e0ab7802
nipunraj <nipunraj99@gmail.com>
2022-12-27 13:14:12 +0530

    v-model for checkbox

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <SignupForm />
+</template>
+
+<script>
+import SignupForm from './components/Signup.vue'
+
+export default {
+  name: 'App',
+  components: { SignupForm },
+}
+</script>
+
+<style scoped></style>
